9bf55d65f20041b62cbfb7a6aeed597ab829a6ba
Jim Hicks <james_hicks@harvard.edu>
2015-05-20 10:30:29 -0400

    adding newrelic support

--- scripts/install_new_relic.sh
+++ scripts/install_new_relic.sh
@@ -4,5 +4,5 @@ rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarc
 yum install -y newrelic-php5 || true
 
 export NR_INSTALL_SILENT=true
-export NR_INSTALL_KEY=your_new_relic_key_here
+export NR_INSTALL_KEY=af94b4d8c7fd7119613fc32428120dff6012faa1
 newrelic-install install
